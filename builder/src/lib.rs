38588aa118505d0024d10248ccf591052cb9f9b7
SangHun Kim <xt.paul23@gmail.com>
2025-09-12 18:23:46 +0900

    builder: 8

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -22,46 +22,57 @@ pub fn derive(input: TokenStream) -> TokenStream {
         let fident = &field.ident;
         let fty = &field.ty;
         if utils::unwrap_option(fty).is_some() {
-            return quote! { #fident: #fty };
+            return Ok(quote! { #fident: #fty });
         }
-        if utils::unwrap_vector(fty).is_some() && utils::unwrap_vec_key(&field.attrs).is_some() {
-            return quote! { #fident: #fty };
+        let each_key = match utils::unwrap_vec_key(&field.attrs) {
+            Err(e) => {
+                return Err(e);
+            }
+            Ok(each_key) => each_key,
+        };
+        if utils::unwrap_vector(fty).is_some() && each_key.is_some() {
+            return Ok(quote! { #fident: #fty });
         }
-        quote! { #fident: Option<#fty> }
+        Ok(quote! { #fident: Option<#fty> })
     });
 
     let builder_fn_impl = named.iter().map(|field| {
         let fident = field.ident.to_owned();
         let fty = field.ty.to_owned();
 
-        let each_key = utils::unwrap_vec_key(&field.attrs);
+        let each_key = match utils::unwrap_vec_key(&field.attrs) {
+            Err(e) => {
+                return Err(e);
+            }
+            Ok(each_key) => each_key,
+        };
 
         if let Some(each_key) = each_key {
             if let Some(ufty) = utils::unwrap_vector(&fty) {
-                return quote! {
+                return Ok(quote! {
                     fn #each_key(&mut self, #each_key: #ufty) -> &mut Self {
                         self.#fident.push(#each_key);
                         self
                     }
-                };
+                });
             }
         }
 
         if let Some(ufty) = utils::unwrap_option(&fty) {
-            return quote! {
+            return Ok(quote! {
                 fn #fident(&mut self, #fident: #ufty) -> &mut Self {
                     self.#fident = Some(#fident);
                     self
                 }
-            };
+            });
         }
 
-        quote! {
+        Ok(quote! {
             fn #fident(&mut self, #fident: #fty) -> &mut Self {
                 self.#fident = Some(#fident);
                 self
             }
-        }
+        })
     });
 
     let unwrap_impl = named.iter().map(|field| {
@@ -69,21 +80,47 @@ pub fn derive(input: TokenStream) -> TokenStream {
         let fident = field.ident.to_owned();
 
         if utils::unwrap_option(fty).is_some() {
-            return quote! {
+            return Ok(quote! {
                 #fident: self.#fident.to_owned()
-            }
+            })
         }
 
-        if utils::unwrap_vector(fty).is_some() && utils::unwrap_vec_key(&field.attrs).is_some() {
-            return quote! {
-                #fident: self.#fident.to_owned()
+        let each_key = match utils::unwrap_vec_key(&field.attrs) {
+            Err(e) => {
+                return Err(e);
             }
+            Ok(each_key) => each_key,
+        };
+
+        if utils::unwrap_vector(fty).is_some() && each_key.is_some() {
+            return Ok(quote! {
+                #fident: self.#fident.to_owned()
+            });
         }
 
-        quote! {
+        Ok(quote! {
             #fident: self.#fident.take().ok_or_else(|| format!("missing field: {}", stringify!(#fident)))?
-        }
+        })
     });
+
+    let option_fields = match option_fields.collect::<Result<Vec<_>, _>>() {
+        Err(e) => {
+            return e.into_compile_error().into();
+        }
+        Ok(x) => x,
+    };
+    let builder_fn_impl = match builder_fn_impl.collect::<Result<Vec<_>, _>>() {
+        Err(e) => {
+            return e.into_compile_error().into();
+        }
+        Ok(x) => x,
+    };
+    let unwrap_impl = match unwrap_impl.collect::<Result<Vec<_>, _>>() {
+        Err(e) => {
+            return e.into_compile_error().into();
+        }
+        Ok(x) => x,
+    };
 
     let tokens = quote! {
         #[derive(Default)]
--- builder/src/utils.rs
+++ builder/src/utils.rs
@@ -50,22 +50,27 @@ pub fn unwrap_vector(ty: &syn::Type) -> Option<syn::Type> {
     Some(gty.to_owned())
 }
 
-pub fn unwrap_vec_key(attrs: &Vec<syn::Attribute>) -> Option<syn::Ident> {
+pub fn unwrap_vec_key(attrs: &Vec<syn::Attribute>) -> Result<Option<syn::Ident>, syn::Error> {
     let mut each_key: Option<syn::Ident> = None;
 
     for attr in attrs {
         if !attr.path().is_ident("builder") {
             continue;
         }
-        let _ = attr.parse_nested_meta(|meta| {
+        if let Err(_) = attr.parse_nested_meta(|meta| {
             if !meta.path.is_ident("each") {
-                return Err(meta.error("unmatched meta ident"));
+                return Err(meta.error("unknown ident"));
             }
             let key = meta.value()?.parse::<syn::LitStr>()?;
             each_key = Some(format_ident!("{}", key.value()));
             Ok(())
-        });
+        }) {
+            return Err(syn::Error::new_spanned(
+                attr.meta.to_owned(),
+                r#"expected `builder(each = "...")`"#,
+            ));
+        }
     }
 
-    each_key
+    Ok(each_key)
 }
