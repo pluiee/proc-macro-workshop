a6c118cd77000372fbec1012f6297584c5d77120
SangHun Kim <xt.paul23@gmail.com>
2026-01-29 17:25:17 +0900

    debug: 4

--- debug/src/lib.rs
+++ debug/src/lib.rs
@@ -1,10 +1,15 @@
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::parse_macro_input;
+use syn::{parse_macro_input, parse_quote, GenericParam, Generics, Meta};
 
-#[proc_macro_derive(CustomDebug)]
+#[proc_macro_derive(CustomDebug, attributes(debug))]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let syn::DeriveInput { ident, data, .. } = parse_macro_input!(input);
+    let syn::DeriveInput {
+        ident,
+        data,
+        generics,
+        ..
+    } = parse_macro_input!(input);
 
     let syn::Data::Struct(syn::DataStruct { fields, .. }) = data else {
         unimplemented!()
@@ -14,16 +19,47 @@ pub fn derive(input: TokenStream) -> TokenStream {
         unimplemented!()
     };
 
-    let field_impl = fields.iter().map(|field| field.ident.to_owned());
+    let generics = add_debug_bounds(generics);
+    let (impl_generics, ty_generics, where_clause) = generics.split_for_impl();
+
+    let field_impl = fields.iter().map(|field| {
+        let ident = field.ident.to_owned();
+        let attrs = field.attrs.to_owned();
+        let mut custom_format = None;
+        for attr in attrs {
+            if !attr.path().is_ident("debug") {
+                continue;
+            }
+            if let Meta::NameValue(nv) = attr.meta {
+                custom_format = Some(nv.value);
+            }
+        }
+        let format_impl = match custom_format {
+            None => quote!("{:?}"),
+            Some(expr) => quote!(#expr),
+        };
+        quote! {
+            .field(stringify!(#ident), &format_args!(#format_impl, self.#ident))
+        }
+    });
 
     quote! {
-        impl std::fmt::Debug for #ident {
+        impl #impl_generics std::fmt::Debug for #ident #ty_generics #where_clause {
             fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::result::Result<(), std::fmt::Error> {
                 f.debug_struct(stringify!(#ident))
-                #(.field(stringify!(#field_impl), &self.#field_impl))* 
+                #(#field_impl)*
                 .finish()
             }
         }
     }
     .into()
+}
+
+fn add_debug_bounds(mut generics: Generics) -> Generics {
+    for param in &mut generics.params {
+        if let GenericParam::Type(ref mut type_param) = *param {
+            type_param.bounds.push(parse_quote!(std::fmt::Debug));
+        }
+    }
+    generics
 }
